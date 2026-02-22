a38aa585edfa5b4ff447c851c7cda20fe769d95c
Zachary Pederson <zap98039@icloud.com>
2026-02-21 21:32:13 -0800

    New modules

--- Exp.lab/QLab1.py
+++ Exp.lab/QLab1.py
@@ -4,8 +4,16 @@ import numpy as np
 from qiskit_aer import AerSimulator
 from qiskit import transpile
 from gates import I, X, Y, Z, H, P, S, T
-from states import ket0, ket1, ket00, ket01, ket10, ket11
 from qiskit.visualization import array_to_latex
+def measure_qc_1024():
+    qc.measure_all()
+    qc.draw(output="text")
+    sim = AerSimulator()
+    t_qc = transpile(qc, sim)
+    result = sim.run(t_qc).result()
+    counts = result.get_counts()
+    print(counts)
+
 
 #CCNOT Gate Circuit on 3 qubits, 1 swapped and 1 superimposed
 #Creates a X(0)*H(1)*CCX(0,1,2) state
@@ -14,14 +22,7 @@ qc.x(0)
 qc.h(1)
 qc.ccx(0, 1, 2)
 
-qc.measure_all()
-qc.draw(output="text")
-
-sim = AerSimulator()
-t_qc = transpile(qc, sim)
-result = sim.run(t_qc).result()
-counts = result.get_counts()
-print(counts)
+measure_qc_1024()
 
 #Rotation Gate Circuit
 qc = QuantumCircuit(2)
@@ -30,15 +31,7 @@ qc = QuantumCircuit(2)
 qc.rz(np.pi/4, 0) #Rotates q0 to hidden state
 #qc.rx(np.pi/4, 0) #Further rotates q0
 qc.h(0) #Creates interferance, converting phase + rotation into measurable bias
-qc.measure_all()
-qc.draw(output="text")
-
-
-sim = AerSimulator()
-t_qc = transpile(qc, sim)
-result = sim.run(t_qc).result()
-counts = result.get_counts()
-print(counts)
+measure_qc_1024()
 
 #Classical Baseline
 qc = QuantumCircuit(2, 2)
@@ -59,28 +52,14 @@ print(counts)
 qc = QuantumCircuit(2)
 qc.h(0)
 qc.cx(0, 1)
-qc.measure_all()
-qc.draw(output="text")
-
-sim = AerSimulator()
-t_qc = transpile(qc, sim)
-result = sim.run(t_qc).result()
-counts = result.get_counts()
-print(counts)
+measure_qc_1024()
 
 #Inteferance Test
 qc = QuantumCircuit(2)
 qc.h(0)
 qc.cx(0, 1)
 qc.h(0)
-qc.measure_all()
-qc.draw(output="text")
-sim = AerSimulator()
-t_qc = transpile(qc, sim)
-result = sim.run(t_qc).result()
-counts = result.get_counts()
-print(counts)
-
+measure_qc_1024()
 qc = QuantumCircuit(2, 2)
 qc.h(0)
 qc.measure(0, 0)
@@ -99,31 +78,46 @@ qc = QuantumCircuit(2)
 qc.h(0)
 qc.cx(0, 1)
 qc.cx(1, 0)
-qc.measure_all()
-qc.draw(output="text")
-sim = AerSimulator()
-t_qc = transpile(qc, sim)
-result = sim.run(t_qc).result()
-counts = result.get_counts()
-print(counts)
+measure_qc_1024()
 
 #Other Tests
-qc = QuantumCircuit(3)
+qc = QuantumCircuit(2)
+qc.x(1)
+qc.cy(1, 0)
 qc.h(0)
-qc.h(1)
-qc.h(2)
-simulator = AerSimulator()
-compiled_circuit = transpile(qc, simulator)
-job = simulator.run(compiled_circuit, shots=1)
-result = job.result()
-statevector = result.get_statevector(qc)
-array_to_latex(statevector, prefix="\text{Statevector= }")
+measure_qc_1024()
 
 
-'''qc.measure_all()
-qc.draw(output="text")
-sim = AerSimulator()
-t_qc = transpile(qc, sim)
-result = sim.run(t_qc).result()
-counts = result.get_counts()
-print(counts)'''
+qc = QuantumCircuit(2)
+qc.h(0) #or x gate
+qc.cx(0, 1)
+qc.cx(1, 0)
+qc.cx(0, 1)
+measure_qc_1024()
+
+
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.swap(0, 1)
+measure_qc_1024()
+
+
+#Toffoli Gate
+qc = QuantumCircuit(3)
+q_0 = 0
+q_1 = 1
+q_2 = 2
+qc.h((q_0, q_1, q_2))
+qc.ccx(q_0, q_1, q_2)
+measure_qc_1024()
+
+#Toffoli Gate Identity
+qc = QuantumCircuit(3)
+q_0 = 0
+q_1 = 1
+q_2 = 2
+qc.h((q_0, q_1, q_2))
+qc.ch(q_0, q_2)
+qc.cz(q_1, q_2)
+qc.ch(q_0, q_2)
+measure_qc_1024()
