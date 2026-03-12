f767b8228eed959c610172387d3da3c77e854d31
Zachary Pederson <28zpederson@overlake.org>
2026-03-11 21:21:13 -0700

    space fix

--- Exp.lab/App.py
+++ Exp.lab/App.py
@@ -277,6 +277,7 @@ def _run_dealer():
 if __name__ == '__main__':
     print('\n  Quantum Blackjack server starting...')
     print('  Open http://localhost:5000 in your browser.\n')
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+app.run(debug=False, host="0.0.0.0", port=port)
 
     
