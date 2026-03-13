daabe2ad76fe208246705e2a185bf7afecd95f00
Zachary Pederson <28zpederson@overlake.org>
2026-03-12 19:48:52 -0700

    Qusic

--- Qusic/quantum_music.py
+++ Qusic/quantum_music.py
@@ -7,7 +7,7 @@ import numpy as np
 def measure_qc_1024():
     qc.measure_all()
     qc.draw(output="text")
-    sim = AerSimulator(shots=10)
+    sim = AerSimulator(shots=32)
     t_qc = transpile(qc, sim)
     result = sim.run(t_qc).result()
     counts = result.get_counts()
@@ -15,48 +15,64 @@ def measure_qc_1024():
     return counts
 
 #QUANTUM CIRCUIT
-qc = QuantumCircuit(4)
+qc = QuantumCircuit(6)
 qc.h(0)
 qc.h(1)
 qc.h(2)
 qc.h(3)
-qc.cx(0, 1)
-qc.cx(1, 2)
-qc.cx(2, 3)
-qc.ry(np.pi/3, 0)
-qc.ry(np.pi/6, 1)
-qc.rz(np.pi/4, 2)
-qc.rz(np.pi/4, 3)
+qc.h(4)
+qc.h(5)
+qc.cx(0, 3)
+qc.cx(1, 4)
+qc.cx(2, 5)
+qc.ry(np.pi/4, 0)
+qc.ry(-np.pi/4, 1)
+qc.ry(np.pi/4, 2)
+qc.rz(-np.pi/6, 3)
+qc.rz(np.pi/3, 4)
+qc.rz(-np.pi/4, 5)
 counts = measure_qc_1024()
 #NOTES
-note_map = {
-    '0000': 48,  # C3 (low)
-    '0001': 50,  # D3
-    '0010': 52,  # E3
-    '0011': 60,  # C4 - chromatic starts here
-    '0100': 61,  # C#4
-    '0101': 62,  # D4
-    '0110': 63,  # D#4
-    '0111': 64,  # E4
-    '1000': 65,  # F4
-    '1001': 66,  # F#4
-    '1010': 67,  # G4
-    '1011': 68,  # G#4
-    '1100': 69,  # A4
-    '1101': 70,  # A#4
-    '1110': 71,  # B4
-    '1111': 72,  # C5 (high)
+melody_map = {
+    '000': 60,  # C4
+    '001': 61,  # C#4
+    '010': 62,  # D4
+    '011': 63,  # D#4
+    '100': 64,  # E4
+    '101': 65,  # F4
+    '110': 66,  # F#4
+    '111': 67,  # G4
 }
-note_sequence = []
+
+accompaniment_map = {
+    '000': 48,  # C3
+    '001': 49,  # C#3
+    '010': 50,  # D3
+    '011': 51,  # D#3
+    '100': 52,  # E3
+    '101': 53,  # F3
+    '110': 54,  # F#3
+    '111': 55,  # G3
+}
+melody_sequence = []
+accompaniment_sequence = []
 for bitstring, count in counts.items():
-    note = note_map[bitstring]
-    note_sequence.extend([note] * count)
+    melody_bits = bitstring[:3]
+    accompaniment_bits = bitstring[3:]
+    print(f"Melody:", melody_sequence)
+    print("Accompaniment:", accompaniment_sequence)
+    melody_sequence.extend([melody_map[melody_bits]] * count)
+    accompaniment_sequence.extend([accompaniment_map[accompaniment_bits]] * count)
 
 #MIDI FILE
-midi = MIDIFile(1)
+midi = MIDIFile(2)
 midi.addTempo(0, 0, 120)
-for i, pitch in enumerate(note_sequence):
+for i, pitch in enumerate(melody_sequence):
     midi.addNote(0, 0, pitch, i, 1, 80)
+
+for i, pitch in enumerate(accompaniment_sequence):
+    midi.addNote(1, 1, pitch, i, 1, 60)
+
 with open("quantum_melody.mid", "wb") as f:
     midi.writeFile(f)
 
