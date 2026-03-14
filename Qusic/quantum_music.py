af059451cb8c31b8afc866867d681b1147902235
Zachary Pederson <zap98039@icloud.com>
2026-03-13 22:22:49 -0700

    all gate maps

--- Qusic/quantum_music.py
+++ Qusic/quantum_music.py
@@ -35,74 +35,111 @@ qc.rz(-np.pi/6, 3)
 qc.rz(np.pi/3, 4)
 qc.rz(-np.pi/4, 5)
 
+#MAP DICTS
+gate_map = {'1': qc.h,'2': qc.x,'3': qc.z}
+
 while not generate_song:
-    superposition_action = input("Would you like to put your melody or accompaniment into superposition? {1. Melody | 2. Accompaniment}")
-    if superposition_action == "1":
-        qc.h(0)
-        qc.h(1)
-        qc.h(2)
-    if superposition_action == "2":
-        qc.h(3)
-        qc.h(4)
-        qc.h(5)
-    melody_action = input("What gates would you like to apply to your melody? {1. H | 2. X | 3. Z | 4. CX | 5. CY | 6. CZ | 7. T}")
-    #H GATE
-    if melody_action == "1":
-        melody_action_H_choice = input("Which qubit would you like to apply an H gate to? {1. 0 | 2. 1 | 3. 2}")
-        qc.h(int(melody_action_H_choice) - 1)
+    action  = input("What would you like to do? {1. Superposition | 2. Apply gate | 3. Generate}")
+    if action == "3":
+        generate_song = True
+        continue
+    if action == "1":
+        superposition_action = input("Would you like to put your melody or accompaniment into superposition? {1. Melody | 2. Accompaniment}")
+        if superposition_action == "1":
+            qc.h(0)
+            qc.h(1)
+            qc.h(2)
+        if superposition_action == "2":
+            qc.h(3)
+            qc.h(4)
+            qc.h(5)
+    if action == "2":
+        action_choice = input("Would you like to apply gates to your melody or accompaniment? {1. M | 2. A}")
+        if action_choice == "1":
+            melody_action = input("What gates would you like to apply to your melody? {1. H | 2. X | 3. Z | 4. CX | 5. CY | 6. CZ | 7. T}")
+            double_gate_map = {'1':(0, 1), '2':(0, 2),'3':(1, 2)}
+            double_gate_choice_1 = {'1':(0, 1),'2':(1, 0)}
+            double_gate_choice_2 = {'1':(0, 2),'2':(2, 0)}
+            double_gate_choice_3 = {'1':(1, 2),'2':(2, 1)}
+            triple_gate_choice = {'1':(0, 1, 2), '2':(0, 2, 1), '3':(1, 2, 0)}
+        if action_choice == '2':
+            double_gate_map = {'1':(3, 4), '2':(3, 5), '3':(4, 5)}
+            double_gate_choice_1 = {'1':(3, 4),'2':(4, 3)}
+            double_gate_choice_2 = {'1':(3, 5),'2':(5, 3)}
+            double_gate_choice_3 = {'1':(4, 5),'2':(5, 4)}
+            triple_gate_choice = {'1':(3, 4, 5), '2':(3, 5, 4), '3':(4, 5, 3)}
 
-    #X GATE
-    if melody_action == "2":
-        melody_action_X_choice = input("Which qubit would you like to apply an X gate to? {1. 0 | 2. 1 | 3. 2}")
-        qc.x(int(melody_action_X_choice) - 1)
-
-    #Z GATE
-    if melody_action == "3":
-        melody_action_Z_choice = input("Which qubit would you like to apply an Z gate to? {1. 0 | 2. 1 | 3. 2}")
-        qc.z(int(melody_action_Z_choice) - 1)
-
+    #H, X, Z GATES
+        if melody_action in gate_map:
+            single_melody_qubit = int(input("Which qubit would you like to target? { 1. 0 | 2. 1 | 3. 2}"))
+            gate_map[melody_action](single_melody_qubit)
     #CX GATE
-    if melody_action == "4":
-        melody_action_CX_choice = input("Which two qubits would you like to entangle? {1. 0 & 1 | 2. 0 & 2 | 3. 1 & 2}")
-        if melody_action_CX_choice == "1":
-            CX_choice_1 = input("Choose target and control qubits {1. Target: 0, Control: 1 | 2. Target: 1, Control: 0}")
-            if CX_choice_1 == "1":
-                qc.cx(0, 1)
-            if CX_choice_1 == "2":
-                qc.cx(1, 0)
-        if melody_action_CX_choice == "2":
-            CX_choice_2 = input("Choose target and control qubits {1. Target: 0, Control: 2 | 2. Target: 2, Control: 0}")
-            if CX_choice_2 == "1":
-                qc.cx(0, 2)
-            if CX_choice_2 == "2":
-                qc.cx(2, 0)
-        if melody_action_CX_choice == "3":
-            CX_choice_3 = input("Choose target and control qubits {1. Target: 1, Control: 2 | 2. Target: 2, Control: 1}")
-            if CX_choice_3 == "1":
-                qc.cx(0, 2)
-            if CX_choice_3 == "2":
-                qc.cx(2, 0)
+        if melody_action == "4":
+            melody_action_CX_choice = input("Which two qubits would you like to entangle? {1. 0 & 1 | 2. 0 & 2 | 3. 1 & 2}")
+            if melody_action_CX_choice == "1":
+                CX_choice_1 = input("Choose target and control qubits {1. Target: 0, Control: 1 | 2. Target: 1, Control: 0}")
+                CX_pair = double_gate_map[melody_action_CX_choice]
+                CX_order = double_gate_choice_1[CX_choice_1]
+                qc.cx(*CX_order)
+            if melody_action_CX_choice == "2":
+                CX_choice_2 = input("Choose target and control qubits {1. Target: 0, Control: 2 | 2. Target: 2, Control: 0}")
+                CX_pair = double_gate_map[melody_action_CX_choice]
+                CX_order = double_gate_choice_1[CX_choice_1]
+                qc.cx(*CX_order)
+            if melody_action_CX_choice == "3":
+                CX_choice_3 = input("Choose target and control qubits {1. Target: 1, Control: 2 | 2. Target: 2, Control: 1}")
+                CX_pair = double_gate_map[melody_action_CX_choice]
+                CX_order = double_gate_choice_1[CX_choice_1]
+                qc.cx(*CX_order)
+
     #CY GATE
-    if melody_action == "5":
-        melody_action_CY_choice = input("Which two qubits would you like to apply a CY gate to? {1. 0 & 1 | 2. 0 & 2 | 3. 1 & 2}")
-        if melody_action_CY_choice == "1":
-            CY_choice_1 = input("Choose target and control qubits {1. Target: 0, Control: 1 | 2. Target: 1, Control: 0}")
-            if CY_choice_1 == "1":
-                qc.cy(0, 1)
-            if CY_choice_1 == "2":
-                qc.cy(1, 0)
-        if melody_action_CY_choice == "2":
-            CY_choice_2 = input("Choose target and control qubits {1. Target: 0, Control: 2 | 2. Target: 2, Control: 0}")
-            if CY_choice_2 == "1":
-                qc.cy(0, 2)
-            if CY_choice_2 == "2":
-                qc.cy(2, 0)
-        if melody_action_CY_choice == "3":
-            CY_choice_3 = input("Choose target and control qubits {1. Target: 1, Control: 2 | 2. Target: 2, Control: 1}")
-            if CY_choice_3 == "1":
-                qc.cy(0, 2)
-            if CY_choice_3 == "2":
-                qc.cy(2, 0)
+        if melody_action == "5":
+            melody_action_CY_choice = input("Which two qubits would you like to apply a CY gate to? {1. 0 & 1 | 2. 0 & 2 | 3. 1 & 2}")
+            if melody_action_CY_choice == "1":
+                CY_choice_1 = input("Choose target and control qubits {1. Target: 0, Control: 1 | 2. Target: 1, Control: 0}")
+                CY_pair = double_gate_map[melody_action_CY_choice]
+                CY_order = double_gate_choice_1[CY_choice_1]
+                qc.cy(*CY_order)
+            if melody_action_CY_choice == "2":
+                CY_choice_2 = input("Choose target and control qubits {1. Target: 0, Control: 2 | 2. Target: 2, Control: 0}")
+                CY_pair = double_gate_map[melody_action_CY_choice]
+                CY_order = double_gate_choice_1[CY_choice_1]
+                qc.cy(*CY_order)
+            if melody_action_CY_choice == "3":
+                CY_choice_3 = input("Choose target and control qubits {1. Target: 1, Control: 2 | 2. Target: 2, Control: 1}")
+                CY_pair = double_gate_map[melody_action_CY_choice]
+                CY_order = double_gate_choice_1[CY_choice_1]
+                qc.cy(*CY_order)
+    #CZ GATE
+        if melody_action == "6":
+            melody_action_CZ_choice = input("Which two qubits would you like to apply a CZ gate to? {1. 0 & 1 | 2. 0 & 2 | 3. 1 & 2}")
+            if melody_action_CZ_choice == "1":
+                CZ_choice_1 = input("Choose target and control qubits {1. Target: 0, Control: 1 | 2. Target: 1, Control: 0}")
+                CZ_pair = double_gate_map[melody_action_CZ_choice]
+                CZ_order = double_gate_choice_1[CZ_choice_1]
+                qc.cz(*CZ_order)
+            if melody_action_CZ_choice == "2":
+                CZ_choice_1 = input("Choose target and control qubits {1. Target: 0, Control: 1 | 2. Target: 1, Control: 0}")
+                CZ_pair = double_gate_map[melody_action_CZ_choice]
+                CZ_order = double_gate_choice_1[CZ_choice_1]
+                qc.cz(*CZ_order)
+            if melody_action_CZ_choice == "3":
+                CZ_choice_1 = input("Choose target and control qubits {1. Target: 0, Control: 1 | 2. Target: 1, Control: 0}")
+                CZ_pair = double_gate_map[melody_action_CZ_choice]
+                CZ_order = double_gate_choice_1[CZ_choice_1]
+                qc.cz(*CZ_order)
+
+    #TOFFOLI
+        if melody_action == "7":
+            melody_action_T_choice = input("Choose target and control qubits {" \
+            "1. Control: 0 & 1, Target: 2 | " \
+            "2. Control: 0 & 2, Target: 1 | " \
+            "3. Control: 1 & 2, Target: 0 | " \
+            "}")
+            T_pair = triple_gate_choice[melody_action_T_choice]
+            qc.ccx(*T_pair)
+
+
 counts = measure_qc_1024()
 melody_map = {
     '000': 60,  # C4
